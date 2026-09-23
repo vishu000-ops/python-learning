27c8f91b4a07d5d1ecef161fa0d9201bbe0b578c
vishu000-ops <vpvivekpanchal1991vp@gmail.com>
2026-09-23 23:33:45 +0530

    countdown

---
+++ timer.py
@@ -0,0 +1,15 @@
+# timer countdown
+
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end='\r')  # Overwrite the line each second
+        time.sleep(1)
+        t -= 1
+
+t = input("Enter the time in seconds: ")
+
+countdown(int(t))
